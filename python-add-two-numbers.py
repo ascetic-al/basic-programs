8d6495a21fb2b81810465441a01ea24e1973ef19
ascetic-al <arobaklub@gmail.com>
2024-06-22 16:11:36 +0200

    Create python-add-two-numbers.py

---
+++ python-add-two-numbers.py
@@ -0,0 +1,6 @@
+#A program that asks the user for two numbers, one after the other, then performs addition of those two numbers and prints the result.
+
+num1 = int(input('First number: '))
+num2 = int(input('Second number: '))
+
+print(num1, '+', num2, '=', num1 + num2)
