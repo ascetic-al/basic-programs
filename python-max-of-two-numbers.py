f58c630c5a34e28ab417ec984c6d4bd677027d51
ascetic-al <arobaklub@gmail.com>
2024-06-22 16:24:14 +0200

    Create python-max-of-two-numbers.py

---
+++ python-max-of-two-numbers.py
@@ -0,0 +1,11 @@
+#A program that asks the user for two numbers, then checks whether the numbers are equal, in which case it prints the string "Those numbers are equal". If they aren't equal, it finds the larger number of those two and prints it.
+
+num1 = int(input('First number: '))
+num2 = int(input('Second number: '))
+
+if num1 == num2:
+    print('Those numbers are equal')
+elif num1 > num2:
+    print(num1)
+else:
+    print(num2)
