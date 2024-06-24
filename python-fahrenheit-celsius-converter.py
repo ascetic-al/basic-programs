9f50408e294fef5f6ae99a5ab6cc66c748dfe690
ascetic-al <arobaklub@gmail.com>
2024-06-24 19:40:02 +0200

    Update python-fahrenheit-celsius-converter.py

--- python-fahrenheit-celsius-converter.py
+++ python-fahrenheit-celsius-converter.py
@@ -2,8 +2,17 @@
 # degrees Fahrenheit to Celsius or the other way around.
 # Then it performs the conversion and prints the result.
 
+# What I did here could be done in way more elegant ways, but I like the simplicity of it.
+# Also a reminder that 0 is a value like any other! (Or is it?)
 choice = int(input('If you want to convert degrees Fahrenheit to Celsius, input 0. Otherwise input 1.'))
 
+# Simple if-elif-else is the main part of this program. 
+# The program first checks if 'choice' is 0,
+# in which case the part below the first conditional, 'if', 
+# is the one that the program executes.
+# If it isn't 0, it checks again whether it's 1, maybe?
+# If it is 1, the program executes the part below the elif conditional.
+# If isn't any of those two, the else part is the one that the program executes.
 if choice == 0:
     degrees_fahrenheit = float(input('How many degrees Fahrenheit to convert to Celsius: '))
     converted_fahrenheit = (degrees_fahrenheit - 32) / 1.8
