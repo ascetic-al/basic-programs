0f3af65d9ff821354a0686e8159fd50eafbd5a80
ascetic-al <arobaklub@gmail.com>
2024-06-24 19:08:18 +0200

    Create python-factorial-of-a-number.py

---
+++ python-factorial-of-a-number.py
@@ -0,0 +1,19 @@
+# A program that asks for a number of which to find factorial,
+# then performs the calculation of the factorial
+# and prints the result.
+
+num = int(input('Type a number of which you want to find factorial: '))
+
+# The 'factorial' variable is needed to keep track of
+# the result of the current calculation in the current iteration of the while loop.
+# It's initialized to 1, because 0! == 1.
+factorial = 1
+
+# The while loop that actually calculates the factorial.
+# A humble reminder that 'factorial *= num' here 
+# is just shortened form of 'factorial = factorial * num'.
+while num > 0:
+    factorial *= num
+    num -= 1
+
+print (factorial)
